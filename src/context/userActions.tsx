ce4f0aa96392e59240f333eeab9ef26beeff8132
Arianna <ariigrangetto5@gmail.com>
2026-04-13 10:38:15 -0300

    feat: add session, event check and functions

--- src/context/userActions.tsx
+++ src/context/userActions.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect } from "react";
 import { supabase } from "../supabase/client.ts";
-import { redirect } from "react-router";
+import { useNavigate } from "react-router";
 
 interface userProviderProps {
     children: React.ReactNode;
@@ -9,18 +9,24 @@ interface userProviderProps {
 interface userContextType {
     login: (email: string, password: string) => Promise<Error | undefined>;
     register: (email: string, name: string, password: string, lastName: string) => Promise<Error | undefined>;
+    signOut: () => Promise<Error | undefined>;
+    resetPassword: (email: string) => Promise<Error | undefined>;
 }
 
 export const UserContext = createContext<userContextType | undefined>(undefined);
 
 export function UserProvider({ children }: userProviderProps) {
+    const navigate = useNavigate();
 
     useEffect(() => {
         const { data } = supabase.auth.onAuthStateChange((event, session) => {
             console.log(session)
             console.log(event)
             if (event === "SIGNED_IN" && session) {
-                redirect("/dashboard");
+                navigate("/");
+            }
+            if (event === "SIGNED_OUT" && session === null) {
+                navigate("/login");
             }
         })
 
@@ -71,8 +77,36 @@ export function UserProvider({ children }: userProviderProps) {
 
     }
 
+    const signOut = async () => {
+        try {
+            const { error } = await supabase.auth.signOut();
+            if (error) {
+                console.error(error.message);
+                return error;
+            }
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    const resetPassword = async (email: string) => {
+        try {
+            const { error } = await supabase.auth.resetPasswordForEmail(email);
+
+            if (error) {
+                console.error(error.message);
+                return error;
+            }
+
+        } catch (error) {
+            throw new Error(error.message);
+        }
+
+
+    }
+
     return (
-        <UserContext.Provider value={{ login, register }}>
+        <UserContext.Provider value={{ login, resetPassword, signOut, register }}>
             {children}
         </UserContext.Provider>
     )
