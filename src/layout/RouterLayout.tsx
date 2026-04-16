eac0a209e9c9c781a360cd027f0036c33eadc3f3
Arianna <ariigrangetto5@gmail.com>
2026-04-16 18:32:47 -0300

    feat: added id prop

--- src/layout/RouterLayout.tsx
+++ src/layout/RouterLayout.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router";
+import { Outlet, } from "react-router";
 import { UserProvider } from "../context/UserActions.tsx";
 import DashboardProvider from "../context/DashboardContext.tsx";
 import { ThemeProvider } from "../context/ThemeProvider.tsx";
 
-export default function RootLayout() {
+export default function RootLayout({ id }: { id: string }) {
     return (
         <ThemeProvider defaultTheme="system">
             <UserProvider>
-                <DashboardProvider>
+                <DashboardProvider id={id}>
                     <Outlet />
                 </DashboardProvider>
             </UserProvider>
