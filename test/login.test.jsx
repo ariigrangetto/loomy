6bcd8a51806e8b32822df02938041590c18aa601
Arianna <ariigrangetto5@gmail.com>
2026-04-21 10:46:52 -0300

    feat: add login page test

---
+++ test/login.test.jsx
@@ -0,0 +1,59 @@
+import { describe, expect, test } from "vitest";
+import { MemoryRouter, Route, Routes } from "react-router";
+import { render, screen } from "@testing-library/react";
+import Login from "../src/pages/Login.tsx"
+import { ThemeProvider } from "../src/context/ThemeProvider.tsx";
+import StateProvider from "../src/context/StateContext.tsx";
+import UserProvider from "../src/context/UserActions.tsx";
+import MatchMediaMock from "vitest-matchmedia-mock";
+
+describe("find google button", () => {
+    test(`Login page with form and google btn`, async () => {
+        let macthMediaMock = new MatchMediaMock();
+        macthMediaMock.useMediaQuery('(prefers-color-scheme: dark)');
+        render(
+            <MemoryRouter initialEntries={["/login"]}>
+                <UserProvider>
+                    <StateProvider>
+                        <ThemeProvider defaultTheme="system">
+                            <Routes>
+                                <Route path="/login" element={<Login />} />
+                            </Routes>
+                        </ThemeProvider>
+                    </StateProvider>
+                </UserProvider>
+            </MemoryRouter>
+        );
+        const googleBtn = screen.getByRole("button", { name: /Continue with Google/i })
+        expect(googleBtn).toBeInTheDocument();
+    });
+});
+
+
+describe("Find login form", () => {
+    test("Login page form", async () => {
+        let matchMediaMock = new MatchMediaMock();
+        matchMediaMock.useMediaQuery('(prefers-color-scheme: dark)');
+        render(
+            <MemoryRouter initialEntries={["/login"]}>
+                <UserProvider>
+                    <StateProvider>
+                        <ThemeProvider defaultTheme="system">
+                            <Routes>
+                                <Route path="/login" element={<Login />} />
+                            </Routes>
+                        </ThemeProvider>
+                    </StateProvider>
+                </UserProvider>
+            </MemoryRouter>
+        );
+        const form = document.querySelector("form");
+        const emailInput = screen.getByLabelText(/Professional Email/i);
+        const passwordInput = screen.getByLabelText(/Password/i);
+        const newInPlatformLink = screen.getByRole("link", { name: /Join Loomy/i })
+        expect(form).toBeInTheDocument();
+        expect(emailInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+        expect(newInPlatformLink).toBeInTheDocument();
+    });
+})
